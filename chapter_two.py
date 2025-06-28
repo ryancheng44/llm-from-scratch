607dc71f52d227ff4c39d3b9fa9c3a3c6f368d89
ryancheng44 <ryancheng44@gmail.com>
2025-06-27 22:55:15 -0400

    Refactor to use tiktoken and add GPTDatasetV1
    Replaces the custom SimpleTokenizerV2 with tiktoken's GPT-2 tokenizer, and introduces a GPTDatasetV1 class for dataset handling. Removes old tokenization logic and streamlines the code for dataset and embedding layer demonstration.

--- chapter_two.py
+++ chapter_two.py
@@ -1,75 +1,11 @@
-import re
+from torch.utils.data import Dataset, DataLoader
+
+import tiktoken
+import torch
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-preprocessed = [item.strip() for item in preprocessed if item.strip()]
-
-all_tokens = sorted(list(set(preprocessed)))
-all_tokens.extend(["<|endoftext|>", "<|unk|>"])
-vocab = {token: integer for integer, token in enumerate(all_tokens)}
-
-
-class SimpleTokenizerV2:
-    def __init__(self, vocab):
-        self.str_to_int = vocab
-        self.int_to_str = {i: s for s, i in vocab.items()}
-
-    def encode(self, text):
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-        preprocessed = [item.strip() for item in preprocessed if item.strip()]
-        preprocessed = [
-            item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
-
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
-
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
-        return text
-
-
-text1 = "Hello, do you like tea?"
-text2 = "In the sunlit terraces of the palace."
-text = " <|endoftext|> ".join((text1, text2))
-
-tokenizer = SimpleTokenizerV2(vocab)
-# print(tokenizer.decode(tokenizer.encode(text)))
-
-# from importlib.metadata import version
-import tiktoken
-# print("tiktoken version:", version("tiktoken"))
-
-tokenizer = tiktoken.get_encoding("gpt2")
-
-text = ("Hello, do you like tea? <|endoftext|> In the sunlit terraces of someunknownPlace.")
-
-integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-# print(integers)
-
-strings = tokenizer.decode(integers)
-# print(strings)
-
-enc_text = tokenizer.encode(raw_text)
-# print(len(enc_text))
-
-enc_sample = enc_text[50:]
-
-context_size = 4
-x = enc_sample[:context_size]
-y = enc_sample[1:context_size+1]
-# print(f"x: {x}")
-# print(f"y: {y}")
-
-# for i in range(1, context_size+1):
-#     context = enc_sample[:i]
-#     desired = enc_sample[i]
-#     print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
-
-import torch
-from torch.utils.data import Dataset, DataLoader
 
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
@@ -86,31 +22,19 @@ class GPTDatasetV1(Dataset):
 
     def __len__(self):
         return len(self.input_ids)
-    
+
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
-    
+
+
 def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
     tokenizer = tiktoken.get_encoding("gpt2")
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
-    dataloader = DataLoader(dataset, batch_size=batch_size, drop_last=drop_last, num_workers=num_workers)
+    dataloader = DataLoader(dataset, batch_size=batch_size,
+                            drop_last=drop_last, num_workers=num_workers)
 
     return dataloader
 
-# dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
-# data_iter = iter(dataloader)
-# first_batch = next(data_iter)
-# print(first_batch)
-
-# second_batch = next(data_iter)
-# print(second_batch)
-
-# dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
-
-# data_iter = iter(dataloader)
-# inputs, targets = next(data_iter)
-# print("Inputs:\n", inputs)
-# print("\nTargets:\n", targets)
 
 input_ids = torch.tensor([2, 3, 5, 1])
 
@@ -119,8 +43,5 @@ output_dim = 3
 
 torch.manual_seed(123)
 embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-# print(embedding_layer.weight)
-
-# print(embedding_layer(torch.tensor([3])))
 
 print(embedding_layer(input_ids))
