459b60594ab9655a63d586a18bf7daf7cd8b2b93
ryancheng44 <ryancheng44@gmail.com>
2025-06-27 22:49:09 -0400

    Add GPTDatasetV1 and dataloader utility for tokenized text
    Introduces a PyTorch Dataset (GPTDatasetV1) and a dataloader creation function for handling tokenized text using tiktoken's GPT-2 encoding. Also adds embedding layer demonstration and various code snippets for tokenization and batching. Minor whitespace fix in the-verdict.txt.

--- chapter_two.py
+++ chapter_two.py
@@ -36,4 +36,91 @@ text2 = "In the sunlit terraces of the palace."
 text = " <|endoftext|> ".join((text1, text2))
 
 tokenizer = SimpleTokenizerV2(vocab)
-print(tokenizer.decode(tokenizer.encode(text)))
+# print(tokenizer.decode(tokenizer.encode(text)))
+
+# from importlib.metadata import version
+import tiktoken
+# print("tiktoken version:", version("tiktoken"))
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = ("Hello, do you like tea? <|endoftext|> In the sunlit terraces of someunknownPlace.")
+
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+# print(integers)
+
+strings = tokenizer.decode(integers)
+# print(strings)
+
+enc_text = tokenizer.encode(raw_text)
+# print(len(enc_text))
+
+enc_sample = enc_text[50:]
+
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+# print(f"x: {x}")
+# print(f"y: {y}")
+
+# for i in range(1, context_size+1):
+#     context = enc_sample[:i]
+#     desired = enc_sample[i]
+#     print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i: i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(dataset, batch_size=batch_size, drop_last=drop_last, num_workers=num_workers)
+
+    return dataloader
+
+# dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+# data_iter = iter(dataloader)
+# first_batch = next(data_iter)
+# print(first_batch)
+
+# second_batch = next(data_iter)
+# print(second_batch)
+
+# dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
+
+# data_iter = iter(dataloader)
+# inputs, targets = next(data_iter)
+# print("Inputs:\n", inputs)
+# print("\nTargets:\n", targets)
+
+input_ids = torch.tensor([2, 3, 5, 1])
+
+vocab_size = 6
+output_dim = 3
+
+torch.manual_seed(123)
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+# print(embedding_layer.weight)
+
+# print(embedding_layer(torch.tensor([3])))
+
+print(embedding_layer(input_ids))
